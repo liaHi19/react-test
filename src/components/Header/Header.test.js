5a49fd0db81533f125cabed62232aa16ed1a98cd
liaHi19 <gn.natalia19@gmail.com>
2022-08-03 14:54:43 +0300

    [#1] Add tests for Header

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+it("should render the same text that into title prop", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+// it("should be a heading", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByRole("heading", { name: "My Header" });
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+// it("should have a title", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByTitle("Header");
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+// it("should have an id", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByTestId("header-1");
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+// //Find by
+
+// it("should render the same text that into prop", async () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = await screen.findByText(/my header/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+// //Query by
+// it("should render the same text", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.queryByText(/dogs/i);
+//   expect(headingElement).not.toBeInTheDocument();
+// });
+
+// //All
+// it("should to be 2 heading", () => {
+//   render(<Header title="My Header" />);
+//   const headingElements = screen.getAllByRole("heading");
+//   expect(headingElements.length).toBe(2);
+// });
